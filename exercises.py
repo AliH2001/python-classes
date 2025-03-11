6e0edfa954f3b6992359adde0cabd2c24249ca45
AliH2001 <ali.hussain.jaffer.2001@gmail.com>
2025-03-11 14:11:05 +0300

    Define a play_game method

--- exercises.py
+++ exercises.py
@@ -8,3 +8,10 @@ class Game():
             'a2': None, 'b2': None, 'c2': None,
             'a3': None, 'b3': None, 'c3': None,
         }
+        
+    def play_game(self):
+        print("Welcome to the game.")
+        
+        
+game_instance = Game()
+game_instance.play_game()
