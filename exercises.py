7a3998fb97c87ab3d7fbc3a06851c0aa4b0bd2af
AliH2001 <ali.hussain.jaffer.2001@gmail.com>
2025-03-11 14:49:17 +0300

    Rendering the board and messages

--- exercises.py
+++ exercises.py
@@ -9,9 +9,34 @@ class Game():
             'a3': None, 'b3': None, 'c3': None,
         }
         
+    def print_board(self):
+     b = self.board
+     print(f"""
+        A   B   C
+    1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
+        ----------
+    2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
+        ----------
+    3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
+          """)
+     
+    def print_message(self):
+        if self.tie:
+            print("Tie game!")
+        elif self.winner:
+            print(f"{self.winner} wins the game!")
+        else:
+            print(f"It's player {self.turn}'s turn!")
+
+
     def play_game(self):
         print("Welcome to the game.")
-        
+        self.print_board()
+        self.print_message() 
+
+
+
         
 game_instance = Game()
 game_instance.play_game()
+
